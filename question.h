eacc025f19d8b8c983b2159f356888886680f798
jessicaminsky <73556745+jessicaminsky@users.noreply.github.com>
2022-04-14 16:22:17 -0400

    Add files via upload

---
+++ question.h
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+class Question{
+    int moneyVal;
+    string question;
+    string answer;
+public:
+    Question(){
+        moneyVal = 0;
+        question = "";
+        answer = "";
+    }
+    Question(int money, string q, string a){
+        moneyVal = money;
+        question = q;
+        answer = a;
+    }
+
+    int getMoneyVal(){
+        return moneyVal;
+    }
+
+    string getQuestion(){
+        return question;
+    }
+
+    string getAnswer(){
+        return answer;
+    }
+};
