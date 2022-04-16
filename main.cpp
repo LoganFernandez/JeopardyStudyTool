845dd50463d0d954b2d15a7cf980ac2895619432
Logan Fernandez <76237263+LoganFernandez@users.noreply.github.com>
2022-04-16 00:18:26 -0400

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,79 +1,137 @@
-#include<bits/stdc++.h>
+#include "heap.h"
+#include <fstream>
+#include <iostream>
+#include <map>
 using namespace std;
 
-// QUESTION OBJECT
-class Question
-{
-public:
-	Question(int _moneyVal, string _question, string _answer);
-private:
-	int moneyVal;
-	string question;
-	string answer;
-};
-Question::Question(int _moneyVal, string _question, string _answer)
-{
-	moneyVal = _moneyVal;
-	question = _question;
-	answer = _answer;
-}
 // read for jeopardy or double jeopardy, money value, questions, and answers
+
 vector<Question> ReadTSV(string file)
+
 {
-	vector<Question> JeopardyVector;
-	ifstream myFile;
-	myFile.open(file);
-	int test = 0;
+    vector<Question> JeopardyVector;
+    ifstream myFile;
+    myFile.open(file);
+    int test = 0;
 
-	// 9 categories
-	string round;
-	string value;
-	int moneyValue;
-	string dailyDouble;
-	string category;
-	string comments;
-	string question;
-	string answer;
-	string airDate;
-	string notes;
-	string line;
-	
-	// take out first line
-	getline(myFile, line);
+    // 9 categories
+    string round;
+    string value;
+    int moneyValue;
+    string dailyDouble;
+    string category;
+    string comments;
+    string question;
+    string answer;
+    string airDate;
+    string notes;
+    string line;
 
-	// push values into Question object vector
-	while (myFile.good() && test != 35) 
-	{
-		getline(myFile, round, '\t');
-		getline(myFile, value, '\t');
-		moneyValue = stoi(value);
-		getline(myFile, dailyDouble, '\t');
-		getline(myFile, category, '\t');
-		getline(myFile, comments, '\t');
-		getline(myFile, question, '\t');
-		getline(myFile, answer, '\t');
-		getline(myFile, line);
+    // take out first line
+    getline(myFile, line);
 
+    // push values into Question object vector
+    while (myFile.good() && test != 1000)
+    {
+        getline(myFile, round, '\t');
+        getline(myFile, value, '\t');
+        moneyValue = stoi(value);
+        getline(myFile, dailyDouble, '\t');
+        getline(myFile, category, '\t');
+        getline(myFile, comments, '\t');
+        getline(myFile, question, '\t');
+        getline(myFile, answer, '\t');
+        getline(myFile, line);
 
-		cout << value << " | " << dailyDouble << ", " << question << " | " << answer << endl;
-		JeopardyVector.push_back(Question(moneyValue, question, answer));
-		test++;
-		cout << endl;
+        //cout << value << " | " << dailyDouble << ", " << question << " | " << answer << endl;
+        JeopardyVector.push_back(Question(moneyValue, question, answer));
+        test++;
 
-	}
-	return JeopardyVector;
+        //cout << endl;
+    }
+    return JeopardyVector;
 }
 
 int main()
 {
-	vector<Question> JeopardyVector = ReadTSV("kids_teen.tsv");
+    vector<Question> JeopardyVector = ReadTSV("kids_teen.tsv");
+    int correct = 0;
 
-	cout << "JeopardyVector size: " << JeopardyVector.size() << endl;
-	cout << endl;
+    //choose a data structure
+    cout << "JEOPARDY STUDY TOOL: CHOOSE A DATA STRUCTURE" << endl << "1. Heap" << endl << "2. Map" << endl;
+    int menuChoice, dataStructure, option, answer, stat;
+    cin >> dataStructure;
 
-	cout << "JEAPARDY STUDY TOOL MENU" << endl << "1. Study Mode" << endl << "2. My Stats" << endl;
-	// 3 CONSECUTIVE CORRECTS BRINGS YOU TO NEXT LEVEL ONE.
-	// 1 INCORRECT RESETS YOUR CONSECUTIVE CHECK.
-	// 2 CONSECUTIVE INCORRECT BRINGS YOU BACK 1. 
-	return 0;
-};
+    //heap
+    if (dataStructure == 1) {
+        //make heap
+        Heap heap(JeopardyVector.size());
+        heap.makeHeap(JeopardyVector);
+
+        //study mode or stats
+        cout << "JEOPARDY STUDY TOOL: MODE" << endl << "1. Study Mode" << endl << "2. My Stats" << endl << "3. Quit" << endl;
+
+        cout << endl;
+
+        cin >> menuChoice;
+
+
+
+        while (menuChoice != 3) {
+            //study mode
+
+            if (menuChoice == 1) {
+
+                cout << "STUDY MODE: CHOOSE ONE" << endl << "1. Read Question" << endl << "2. Advance Level" << endl << "3. Back" << endl;
+                cout << endl;
+                int option;
+                cin >> option;
+
+                while (option != 3) {
+                    //read question
+                    if (option == 1) {
+                        Question q = heap.getQuestion();
+                        cout << "Difficulty: " << q.getMoneyVal() << endl << q.getQuestion() << "?" << endl;
+                        cout << "Press Any Button To Display Answer" << endl;
+                        int answer;
+                        cin >> answer;
+                        cout << q.getAnswer() << endl;
+                        cout << "Was Your Answer Correct?" << endl << "1. Yes" << endl << "2. No" << endl;
+                        cin >> stat;
+                        if (stat == 1) {
+                            correct++;
+                        }
+                        if (stat == 2) {
+                            correct = 0;
+                        }
+
+                        //3 consecutive correct answers advances level
+                        if (correct == 3) {
+                            heap.advanceLevel(heap.getMin().getMoneyVal(), 0);
+                        }
+                    }
+
+                    //advance level
+                    else if (option == 2) {
+                        heap.advanceLevel(heap.getMin().getMoneyVal(), 0);
+                        cout << "New Level: " << heap.getMin().getMoneyVal() << endl;
+                    }
+
+                    //print choices again: read or advance (heap)
+                    cout << "STUDY MODE: CHOOSE ONE" << endl << "1. Read Question" << endl << "2. Advance Level" << endl << "3. Back" << endl;
+                    cin >> option;
+                }
+            }
+
+            //my stats
+            else if (menuChoice == 2) {
+                cout << "Correct Answers: " << correct << endl << "Level: " << heap.getMin().getMoneyVal() << endl;
+            }
+
+            //print choices again: study mode or stats (heap)
+            cout << "JEOPARDY STUDY TOOL: MODE" << endl << "1. Study Mode" << endl << "2. My Stats" << endl << "3. Quit" << endl;
+            cin >> menuChoice;
+        }
+    }
+    return 0;
+}
